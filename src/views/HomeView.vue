fb7c5932d0424c778f14fc02ad38756a91c91f0b
cmchang <cmchang120956@gmail.com>
2022-06-21 06:09:25 +0000

    add vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-alert type="success"></v-alert>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
